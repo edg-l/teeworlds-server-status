f596c4754c86259704e5b48c341eed99a3b126bf
Edgar <git@edgarluque.com>
2018-08-20 23:33:21 +0200

    add async

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,8 @@ var server1 = new ServerInfo('ger.ddnet.tw', 8303, 5000)
 
 server1.on('info', () => {
   console.log(server1)
-})
+});
 
-server1.startSending(() => console.log('Ready!'))
+(async () => {
+  await server1.startSending();
+})()
